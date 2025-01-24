10eeec8b5233f69984a027b7c439c00e7860dc9b
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2025-01-24 17:35:57 +0300

    Change git art

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import { SportInputs } from './SportInputs';
 import { Sport } from './Sport';
@@ -23,32 +22,6 @@ function App() {
       <SportInputs submit={submit}/>
       <Sport data={data} del={del} />
     </div>
-=======
-
-import './App.css';
-import { Color } from './Color';
-import { useState } from 'react';
-
-function App() {
-  let rgb = '';
-  function hex2rgb(c) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(c);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
-  const [color,setColor]=useState('')
-  function colorChange (e) {
-    const {name,value} = e.target;
-    setTimeout(()=>{
-      setColor((form)=>(hex2rgb(value)));
-    },2000)
-  }
-  return (
-    <Color color={color} funk={colorChange}/>
->>>>>>> cab899dd5966dfb4e84903a1074154cc7a801f0c
   );
 }
 
